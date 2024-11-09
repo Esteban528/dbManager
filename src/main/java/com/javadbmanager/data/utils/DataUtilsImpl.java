fdee087e8f73bc4d5d5fdfc3523c215da9e72e31
Esteban Gonzalez Florez <estebangonza2005@gmail.com>
2024-11-09 16:23:36 -0500

    feat: 🐛 Add postgresql suppport

--- src/main/java/com/javadbmanager/data/utils/DataUtilsImpl.java
+++ src/main/java/com/javadbmanager/data/utils/DataUtilsImpl.java
@@ -10,39 +10,48 @@ import java.util.Map;
 import com.javadbmanager.data.ConnectionBean;
 
 public class DataUtilsImpl implements DataUtils {
+    private final Map<String, String> databaseEngineMap = Map.of(
+            "mysql", "jdbc:mysql://%s:%s/%s?useSSL=false&%s",
+            "postgresql", "jdbc:postgresql://%s:%s/%s?ssl=false&%s"
+        );
 
-  @Override
-  public String makeUrl(ConnectionBean connectionBean) {
-    String url, properties = "&useTimezone=true&serverTimezone=UTC";
+    @Override
+    public String makeUrl(ConnectionBean connectionBean) {
+        String url, properties = "&useTimezone=true&serverTimezone=UTC";
 
-    if (connectionBean.getDBversion() >= 8) {
-      properties += "&allowPublicKeyRetrieval=true";
-    }
-
-    url = String.format("jdbc:mysql://%s:%s/%s?useSSL=false&%s",
-        connectionBean.getHost(), connectionBean.getPort(), connectionBean.getDatabase(), properties);
-
-    return url;
-  }
-
-  @Override
-  public Map<String, String> getColumnsData(Connection connection, String tableName) {
-    Map<String, String> columnMap = new HashMap<>();
-
-    try {
-      DatabaseMetaData metaData = (DatabaseMetaData) connection.getMetaData();
-
-      try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
-        while (columns.next()) {
-          String columnName = columns.getString("COLUMN_NAME");
-          String columnType = columns.getString("TYPE_NAME");
-          int columnSize = columns.getInt("COLUMN_SIZE");
-          columnMap.put(columnName, String.format("%s ($d)", columnType, columnSize));
+        if (connectionBean.getDatabase().equals("mysql") && connectionBean.getDBversion() >= 8) {
+            properties += "&allowPublicKeyRetrieval=true";
         }
-      }
-    } catch (SQLException e) {
+
+        url = String.format(
+            databaseEngineMap.getOrDefault(connectionBean.getDBType(), databaseEngineMap.get("mysql")),
+            connectionBean.getHost(),
+            connectionBean.getPort(),
+            connectionBean.getDatabase(),
+            properties
+        );
+
+        return url;
     }
 
-    return columnMap;
-  }
+    @Override
+    public Map<String, String> getColumnsData(Connection connection, String tableName) {
+        Map<String, String> columnMap = new HashMap<>();
+
+        try {
+            DatabaseMetaData metaData = (DatabaseMetaData) connection.getMetaData();
+
+            try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
+                while (columns.next()) {
+                    String columnName = columns.getString("COLUMN_NAME");
+                    String columnType = columns.getString("TYPE_NAME");
+                    int columnSize = columns.getInt("COLUMN_SIZE");
+                    columnMap.put(columnName, String.format("%s ($d)", columnType, columnSize));
+                }
+            }
+        } catch (SQLException e) {
+        }
+
+        return columnMap;
+    }
 }
